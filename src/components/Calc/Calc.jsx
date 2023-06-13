2a495e9bd2f9802a5e2584a54eb1de054cfa8192
Mathivanan09 <vmathivananbe@gmail.com>
2023-06-14 01:56:51 +0530

    Common Layout changes

--- src/components/Calc/Calc.jsx
+++ src/components/Calc/Calc.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../../styles/Styles.css';
 
 const Calc = () => {
   return (
-    <div>
-        
+    <div className='calc_container'>
+      <div className='row p-5 justify-content-center'>
+        CALCULATOR JS
+      </div>
+      <div className='row justify-content-center'>
+        <div className='col col-7 border border-secondary border-3'>
+          <div className='row d-inline'>
+ 
+          </div>
+          <div className='row d-inline'>
+            
+          </div>
+          <div className='row d-inline'>
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
