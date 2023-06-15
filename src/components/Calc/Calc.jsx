4683f51ed87486d06e807a80988b23a01907b8ba
Mathivanan09 <vmathivananbe@gmail.com>
2023-06-16 00:45:30 +0530

    Update and evaluate input values with Clear button

--- src/components/Calc/Calc.jsx
+++ src/components/Calc/Calc.jsx
@@ -4,10 +4,33 @@ import labels from '../../constants/default';
 
 const Calc = () => {
 
-  const [inputs, setInputs] = useState('6 + 8');
-  const [result, setResult] = useState(6 + 8);
+  const [inputs, setInputs] = useState('0');
+  const [result, setResult] = useState(0);
 
   const inputSymbols = labels.inputSymbols;
+  const specialChar = ['%', 'X', '+', '-', '/', '='];
+  const handleResponse = (e) => {
+    // console.log(inputs.length);
+    if (e.target.value === 'AC') {
+      setInputs('0');
+      setResult(0);
+    } else {
+      let content = '';
+      if ((inputs.length === 1) && (inputs === '0')) {
+        content += e.target.value;
+      } else {
+        content = inputs + e.target.value;
+      }
+      setInputs(content);
+      console.log(content);
+      if (!specialChar.includes(e.target.value)) {
+        const evaluvatedData = eval(content);
+        setResult(evaluvatedData);
+      }
+    }
+
+  }
+  // console.log(inputs);
 
   return (
     <div className='calc-container'>
@@ -15,7 +38,7 @@ const Calc = () => {
         CALCULATOR JS
       </div>
       <div className='row justify-content-center'>
-        <div className='col col-6 border border-secondary border-3'>
+        <div className='col col-5 border border-secondary border-3'>
           <div className='row input-container justify-content-end'>
             {inputs}
             <br />
@@ -28,7 +51,7 @@ const Calc = () => {
             <div className='row p-1' key={idx}>
               {lineSymbol?.map((symbols, sdx) => (
                 <div className='d-grid gap-2 col-3 mx-auto' key={sdx}>
-                  <button className='btn btn-outline-secondary'>{symbols}</button>
+                  <button onClick={handleResponse} value={symbols} className='btn btn-outline-secondary'>{symbols}</button>
                 </div>
               ))}
             </div>
